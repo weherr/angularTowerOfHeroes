ae5c8a5d4271bed9bcbcc9aba0d80c51acd2bea2
Billy <wherr.1994@gmail.com>
2020-08-21 15:18:54 +0700

    Added a list of heroes in a seperate file and display them using *ngFor. Added a selectedHero only shown when one hero is selected

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -13,10 +14,27 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //the starting selected hero is blank of type Hero
+  selectedHero: Hero;
+  // selectedHero : Hero = {
+  // 	id: 99,
+  // 	name: 'first hero'
+  // };
+
+
+
+  //onSelect function that takes in a Hero we shall refer to as newlySelectedHero in the function
+  onSelect(newlySelectedHero : Hero): void{
+  	this.selectedHero = newlySelectedHero;
+  }
+
+  // the HEROES array is created in the file ../mock-heroes, here we assign it to a variable accessable to the HeroesComponent
+  heroes = HEROES;
+
+  //creating just a starter hero of type Hero
   hero : Hero = {
   	id: 1,
   	name: 'windstorm'
   }
-
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+	{ id: 11, name: 'Hero Man' },
+  	{ id: 12, name: 'Hero Dude' },
+  	{ id: 13, name: 'Im a Hero' },
+  	{ id: 14, name: 'Celeritas' },
+  	{ id: 15, name: 'Magneta' },
+  	{ id: 16, name: 'RubberMan' },
+  	{ id: 17, name: 'Dynama' },
+  	{ id: 18, name: 'Dr IQ' },
+  	{ id: 19, name: 'Magma' },
+  	{ id: 20, name: 'Tornado' },
+  	// gave same ID to see what happens
+  	{ id: 20, name: 'Super Hero'}
+]
