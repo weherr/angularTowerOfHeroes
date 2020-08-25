f88913a2bc7d97d9aaeabfe7c39e39c5b939d72b
Billy <wherr.1994@gmail.com>
2020-08-25 12:48:53 +0700

    Added messages component, made the mock-heroes come from an observable so it simulates how a server call would need to work.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +11,11 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService : HeroService, private messageService : MessageService) { }
 
   ngOnInit(): void {
+  	//call the getHeroes function
+  	this.getHeroes();
   }
 
   //the starting selected hero is blank of type Hero
@@ -25,16 +29,23 @@ export class HeroesComponent implements OnInit {
 
   //onSelect function that takes in a Hero we shall refer to as newlySelectedHero in the function
   onSelect(newlySelectedHero : Hero): void{
+  	this.messageService.add(newlySelectedHero.name + ' was selected');
   	this.selectedHero = newlySelectedHero;
   }
 
-  // the HEROES array is created in the file ../mock-heroes, here we assign it to a variable accessable to the HeroesComponent
-  heroes = HEROES;
+  // the HEROES array is created in the file ../mock-heroes. Here we put it is a null to start
+  // in the getHeroes() function we have the service go get it
+  heroes = null;
 
   //creating just a starter hero of type Hero
   hero : Hero = {
   	id: 1,
   	name: 'windstorm'
   }
+
+  getHeroes() : void{
+  	this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
 
 }
